8f125d4cbc3773998accdeb6e6ec30eeb5d5f7ad
koechlm <markus.koechl@autodesk.com>
2020-04-15 17:27:01 +0200

    initial

--- Autodesk.VltInvSrv.iLogicSampleJob/Properties/AssemblyInfo.cs
+++ Autodesk.VltInvSrv.iLogicSampleJob/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("API Onboarding - Custom Job")]
+[assembly: AssemblyTitle("Autodesk iLogic Job Extension")]
 [assembly: AssemblyDescription("API Sample - iLogic Rule Execution for Job Processing")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Autodesk")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2020.2.0.1")]
-[assembly: AssemblyFileVersion("2020.2.0.1")]
+[assembly: AssemblyVersion("2021.0.0.0")]
+[assembly: AssemblyFileVersion("2021.0.0.0")]
